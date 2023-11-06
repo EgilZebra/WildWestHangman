1f668668a6baffc167e68569bc31308eb2048ba1
Martin <martin.kolberg@outlook.com>
2023-11-06 16:19:17 +0100

    One honest day´s work

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-let counter = 0;
+const wordList = ["dog", "bear", "horse"];
+const attempts = 8; 
+//declaring multiple variables in one line
+let secretWord, selectedWord, remainingAttempts, wrongGuess
 
-let secretWords = [
-    "camel",
-    "bear",
-    "horse"
-];
-let choosenWord = [0];
-let wrongGuess = [0];
+const wordDisplay = document.getElementById("guessWord");
+//console.log(wordDisplay)
 
-const letterGuess = () => {
-    const i = Math.floor(Math.random() * secretWords.length);
-    return secretWords[i];
+
+// randomize a word from secretWords-array
+const randomizedWord = () => {
+    selectedWord = wordList[Math.floor(Math.random() * wordList.length)];
+    return selectedWord;
+    
+};
+console.log(randomizedWord()) 
+
+//function for displaying empty letter "__", joinas på mellanrum -> till array
+
+    const hideWord = () => {
+    secretWord = Array(selectedWord.length).fill("_");
+    guessWord.textContent = secretWord.join(" ");
+    return secretWord;
 }
-while (counter < secretWords.length) {
-    counter++
+console.log(hideWord());
+
+
+const compareInput = () => {
+    for ( let i = 0; i < selectedWord.length; i++) {
+    if (selectedWord.includes(document.getElementsByClassName("guessbox__input")[0].value, i)) { 
+        console.log(`Ja, det funkar!`)
+    } else {
+        console.log(`Det funkar inte`)
+    }
 }
 
-console.log(letterGuess())
+}
+
+/* 
+secretWord = Array(selectedWord.length).fill("_");
+
+    //
+    guessWord.textContent = secretWord.join(" ");
+ */
+
+/* function impliedLength () {
+     guessWord.textContent = secretWord.join(" ");
+
+} */
+
+
+//shows the number of underscores needed for secretWord
+
+
+//Create function for comparing correct input with several if/else statements - to change the `_`
+/* function userInput (){
+    if (secretWord ==  )
+} */
+
+// 
+
+
+//
+
+/* 
+
+console.log(selectedWord)
+ */
